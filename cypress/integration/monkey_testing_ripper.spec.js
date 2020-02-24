577856b3ff4c9afcde3700a81d876be98f150950
angelaanaya2309 <angela.anaya.castaneda@gmail.com>
2020-02-23 23:59:22 -0500

    cambios

--- cypress/integration/monkey_testing_ripper.spec.js
+++ cypress/integration/monkey_testing_ripper.spec.js
@@ -17,21 +17,49 @@ function randomClick(monkeysLeft,event) {
 
   var monkeysLeft = monkeysLeft;
   if(monkeysLeft > 0) {
+    if(event=='a' || event=='button' )  {
       cy.get(event).then($links => {
           var randomLink = $links.get(getRandomInt(0, $links.length));
           if(!Cypress.dom.isHidden(randomLink)) {
-            if(event=='a' || event=='button' )  {
-              cy.wrap(randomLink).click({force: true});
-            }
-            if(event=='input')  {
+            cy.wrap(randomLink).click({force: true});
+            monkeysLeft = monkeysLeft - 1;
+           /* if(event=='input')  {
               cy.wrap(randomLink).click({force: true}).type("prueba1");
             }
+            if(event=='select')  {
+              cy.wrap(randomLink).select($selects =>) var user;
+            }*/
               
           }
-          monkeysLeft = monkeysLeft - 1;
           cy.wait(1000);
           randomClick(monkeysLeft,event);
       });
+          
+    }
+    if(event=='input' )  {
+      cy.get(event).then($links => {
+          var randomLink = $links.get(getRandomInt(0, $links.length));
+          if(!Cypress.dom.isHidden(randomLink)) {
+           cy.wrap(randomLink).click({force: true}).type("prueba1");
+           monkeysLeft = monkeysLeft - 1;
+          }
+          cy.wait(1000);
+          randomClick(monkeysLeft,event);
+      });
+          
+    }
+    if(event=='select' )  {
+      cy.get(event).then($links => {
+        var randomLink = $links.get(getRandomInt(0, $links.length));
+        if(!Cypress.dom.isHidden(randomLink)) {
+         cy.wrap(randomLink).select([0]);
+         monkeysLeft = monkeysLeft - 1;
+        }
+        cy.wait(1000);
+        randomClick(monkeysLeft,event);
+    });
+          
+    }
   }   
 }
 
